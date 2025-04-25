1917ba84bc5d487efdfc16e4b4f7ddea3ab4d159
tonrock00 <71489861+tonrock00@users.noreply.github.com>
2025-04-25 16:01:01 +0700

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import FacilitiesSection from "./components/FacilitiesSection";
 import LocationSection from "./components/LocationSection";
 import ContactSection from "./components/ContactSection";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="font-sans text-gray-800">
+      <Analytics />
       <Navbar />
       <HeroSection />
       <RoomsSection />
